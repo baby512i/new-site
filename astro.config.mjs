ebbff39acf3599d9c306013dfa7cf38003da8a94
baby512i <majid.missile@gmail.com>
2026-05-04 11:46:38 +0330

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://yourdomain.com',
+
+  output: 'static',
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: 'standalone',
+  }),
+
+  integrations: [react(), sitemap()],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 });
